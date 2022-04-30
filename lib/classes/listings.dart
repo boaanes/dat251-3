b70fd3edf0f07e4a57069a0e1b0411f638a9faa4
Kristian Avlesbug <kristian.avlebsug@student.uib.no>
2022-04-30 17:05:53 +0200

    Added publishing functionality

--- lib/classes/listings.dart
+++ lib/classes/listings.dart
@@ -2,35 +2,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Listings {
   final String listingID;
+  final String userID;
   final String title;
   final String description;
-  final DateTime availableFrom;
-  final DateTime availableTo;
   final String category;
   final int price;
-  final List gear;
 
   Listings(
       {required this.listingID,
+      required this.userID,
       required this.title,
       required this.description,
-      required this.availableFrom,
-      required this.availableTo,
       required this.category,
-      required this.price,
-      required this.gear});
+      required this.price});
 
   factory Listings.fromFirestore(DocumentSnapshot doc) {
     Map data = doc.data()! as Map;
     return Listings(
         listingID: doc.id,
+        userID: data['userID'] ?? 'userID',
         title: data['title'] ?? 'Title',
         description: data['description'] ?? 'Description',
-        availableFrom: data['availableFrom'].toDate() ?? DateTime.now(),
-        availableTo: data['availableTo'].toDate() ?? DateTime.now(),
         category: data['category'] ?? 'Category',
-        price: data['price'] ?? 'Price',
-        gear: data['gear'] ?? 'Gear');
+        price: data['price'] ?? 'Price');
   }
 
   String getLisitngID() {
--- lib/pages/createListing_page.dart
+++ lib/pages/createListing_page.dart
@@ -1,9 +1,18 @@
+import 'dart:html';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:utstyr/classes/listings.dart';
 import 'package:utstyr/pages/login_page.dart';
+import 'package:utstyr/services/firestore_services.dart';
 import 'package:utstyr/widgets/bottombar.dart';
 import 'package:utstyr/widgets/scaffold.dart';
+import 'package:uuid/uuid.dart';
+
+import '../widgets/standardNavigator.dart';
+import 'account_page.dart';
 
 class CreateListing extends StatefulWidget {
   const CreateListing({Key? key}) : super(key: key);
@@ -17,6 +26,12 @@ class _CreateListingState extends State<CreateListing> {
   bool packagePrice = false;
   bool oneDayPrice = false;
   bool oneWeekPrice = false;
+  TextEditingController titleInput = TextEditingController();
+  TextEditingController descriptionInput = TextEditingController();
+  TextEditingController priceInput = TextEditingController();
+  TextEditingController categoryInput = TextEditingController();
+  DateTime date = DateTime.now();
+  var uuid = Uuid();
   @override
   Widget build(BuildContext context) {
     var user = Provider.of<User?>(context);
@@ -60,7 +75,7 @@ class _CreateListingState extends State<CreateListing> {
                                         Container(
                                           width: 500,
                                           child: TextFormField(
-                                            validator: (value) {},
+                                            controller: titleInput,
                                             cursorColor: Theme.of(context)
                                                 .colorScheme
                                                 .secondary,
@@ -102,6 +117,55 @@ class _CreateListingState extends State<CreateListing> {
                                           padding: const EdgeInsets.fromLTRB(
                                               0, 25, 0, 10),
                                           child: Text(
+                                            'Kategori',
+                                            style: TextStyle(fontSize: 18),
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 500,
+                                          child: TextFormField(
+                                            controller: categoryInput,
+                                            cursorColor: Theme.of(context)
+                                                .colorScheme
+                                                .secondary,
+                                            decoration: InputDecoration(
+                                                focusColor: Theme.of(context)
+                                                    .colorScheme
+                                                    .secondary,
+                                                filled: true,
+                                                enabledBorder: OutlineInputBorder(
+                                                    borderSide: BorderSide(
+                                                        color: Theme.of(context)
+                                                            .colorScheme
+                                                            .secondary),
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                                focusedBorder: OutlineInputBorder(
+                                                    borderSide: BorderSide(
+                                                        width: 2,
+                                                        color: Theme.of(context)
+                                                            .colorScheme
+                                                            .secondary),
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            10)),
+                                                fillColor: Colors.grey[50],
+                                                border: const OutlineInputBorder(),
+                                                hintText: "Eks. Friluftsliv",
+                                                hintStyle: TextStyle(color: Colors.grey[400])),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              0, 25, 0, 10),
+                                          child: Text(
                                             'Beskrivelse',
                                             style: TextStyle(fontSize: 18),
                                           ),
@@ -109,7 +173,7 @@ class _CreateListingState extends State<CreateListing> {
                                         Container(
                                           width: 500,
                                           child: TextFormField(
-                                            validator: (value) {},
+                                            controller: descriptionInput,
                                             cursorColor: Theme.of(context)
                                                 .colorScheme
                                                 .secondary,
@@ -145,8 +209,67 @@ class _CreateListingState extends State<CreateListing> {
                                       ],
                                     ),
                                     Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
-                                        Text("Valg"),
+                                        Padding(
+                                          padding: const EdgeInsets.fromLTRB(
+                                              0, 25, 0, 10),
+                                          child: Text(
+                                            'Pris',
+                                            style: TextStyle(fontSize: 18),
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 80,
+                                          child: TextFormField(
+                                            controller: priceInput,
+                                            cursorColor: Theme.of(context)
+                                                .colorScheme
+                                                .secondary,
+                                            maxLines: 10,
+                                            decoration: InputDecoration(
+                                              focusColor: Theme.of(context)
+                                                  .colorScheme
+                                                  .secondary,
+                                              filled: true,
+                                              enabledBorder: OutlineInputBorder(
+                                                  borderSide: BorderSide(
+                                                      color: Theme.of(context)
+                                                          .colorScheme
+                                                          .secondary),
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10)),
+                                              focusedBorder: OutlineInputBorder(
+                                                  borderSide: BorderSide(
+                                                      width: 2,
+                                                      color: Theme.of(context)
+                                                          .colorScheme
+                                                          .secondary),
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10)),
+                                              fillColor: Colors.grey[50],
+                                              border:
+                                                  const OutlineInputBorder(),
+                                              hintText: "300",
+                                              hintStyle: TextStyle(
+                                                  color: Colors.grey[400]),
+                                            ),
+                                            keyboardType: TextInputType.number,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          standardNavigator(
+                                              context, MyAccount());
+                                        },
+                                        child: const Text('Legg til bilde')),
+                                    Column(
+                                      children: [
                                         Row(
                                           children: [
                                             Checkbox(
@@ -221,6 +344,24 @@ class _CreateListingState extends State<CreateListing> {
                                         )
                                       ],
                                     ),
+                                    ElevatedButton(
+                                        //new Listings(listingID: "custom id", title: , description: description, availableFrom: availableFrom, availableTo: availableTo, category: category, price: price, gear: gear)
+                                        onPressed: () {
+                                          Listings listing = new Listings(
+                                              listingID: uuid.v4(),
+                                              userID: FirebaseAuth
+                                                  .instance.currentUser!.uid,
+                                              title: titleInput.text,
+                                              description:
+                                                  descriptionInput.text,
+                                              category: categoryInput.text,
+                                              price:
+                                                  stringToInt(priceInput.text));
+                                          FirestoreServices()
+                                              .postListing(listing);
+                                          print(listing.getDescription());
+                                        },
+                                        child: const Text('Publiser annonse')),
                                   ],
                                 ),
                               )),
@@ -238,4 +379,8 @@ class _CreateListingState extends State<CreateListing> {
             warningMsg: 'listing',
           );
   }
+}
+
+int stringToInt(String price) {
+  return int.parse(price);
 }
